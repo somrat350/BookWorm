442430b94970f6e3d1964463984cf205a00a6631
somrat350 <mdsomratsordaro350@gmail.com>
2026-01-13 09:05:31 +0600

    created proxy for protect route

---
+++ src/proxy.js
@@ -0,0 +1,23 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+const privateRoutes = ["/dashboard"];
+export async function proxy(req) {
+  const reqPath = req.nextUrl.pathname;
+  const token = await getToken({ req });
+  const isAuthenticated = !!token;
+  const isPrivateRoute = privateRoutes.some((route) =>
+    reqPath.startsWith(route)
+  );
+  if (isPrivateRoute && !isAuthenticated) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("callbackUrl", reqPath);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/dashboard/:path*",
+};
